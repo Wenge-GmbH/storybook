6d2ce0f0488ba85b4cc019cf3aee968bebdce067
XImOnTRiX <domenik.wenghofer@gmail.com>
2019-03-26 15:51:33 +0100

    more tab stories

--- src/stories/tabs.stories.js
+++ src/stories/tabs.stories.js
@@ -4,22 +4,41 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Tabs, Heading } from './components';
+import { Tabs, Heading, Button } from './components';
 
 import './css-lib/index.min.css';
 
 const Primary = () => (
   <div>
     <Tabs>
-      <div label="test1">
+      <div label={<p>test 1</p>}>
         <Heading type={1}>Title 1</Heading>
         <p>My content in the first tab</p>
       </div>
-      <div label="test2">
+      <div label=<p>test 2</p>>
         <Heading type={1}>Title 2</Heading>
         <p>My content in the second tab</p>
       </div>
-      <div label="test3">
+      <div label=<p>test 3</p>>
+        <Heading type={1}>Title 3</Heading>
+        <p>My content in the third tab</p>
+      </div>
+    </Tabs>
+  </div>
+);
+
+const WithButtons = () => (
+  <div>
+    <Tabs animation>
+      <div label={<Button primary label="Tab 1" />}>
+        <Heading type={1}>Title 1</Heading>
+        <p>My content in the first tab</p>
+      </div>
+      <div label={<Button primary label="Tab 2" />}>
+        <Heading type={1}>Title 2</Heading>
+        <p>My content in the second tab</p>
+      </div>
+      <div label={<Button primary label="Tab 3" />}>
         <Heading type={1}>Title 3</Heading>
         <p>My content in the third tab</p>
       </div>
@@ -29,3 +48,4 @@ const Primary = () => (
 
 storiesOf('Tabs', module)
   .add('primary', () => (<Primary />))
+  .add('with buttons', () => (<WithButtons />))
