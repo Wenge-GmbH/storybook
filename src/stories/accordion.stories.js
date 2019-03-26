1e017e4220c13da8a3fea203f3c9578c98b98228
XImOnTRiX <domenik.wenghofer@gmail.com>
2019-03-26 15:50:48 +0100

    more stories

--- src/stories/accordion.stories.js
+++ src/stories/accordion.stories.js
@@ -11,6 +11,39 @@ import './css-lib/index.min.css';
 class Primary extends React.Component {
   render() {
     return (
+      <Accordion>
+        <div
+          label={
+            <Heading type={1} className="middle padding bg-tertiary primary">Panel 1</Heading>
+          }
+        >
+          <Heading type={6} className="small padding nopb">sub title</Heading>
+          <p className="padding nopt">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Ste</p>
+        </div>
+        <div
+          label={
+            <Heading type={1} className="middle padding bg-tertiary primary">Panel 2</Heading>
+          }
+        >
+          <Heading type={6} className="small padding nopb">sub title</Heading>
+          <p className="padding nopt">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Ste</p>
+        </div>
+        <div
+          label={
+            <Heading type={1} className="middle padding bg-tertiary primary">Panel 3</Heading>
+          }
+        >
+          <Heading type={6} className="small padding nopb">sub title</Heading>
+          <p className="padding nopt">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Ste</p>
+        </div>
+      </Accordion>
+    );
+  }
+}
+
+class Toggle extends React.Component {
+  render() {
+    return (
       <Accordion toggle>
         <div
           label={
@@ -43,3 +76,4 @@ class Primary extends React.Component {
 
 storiesOf('Accordion', module)
   .add('primary', () => (<Primary />))
+  .add('toggle', () => (<Toggle />))
