9954a3acc5d99d9f6c704686271483516337f1a0
XImOnTRiX <domenik.wenghofer@gmail.com>
2019-03-26 12:00:16 +0100

    added export & imports

--- src/stories/components/index.js
+++ src/stories/components/index.js
@@ -2,10 +2,12 @@ import { Button } from './buttons';
 import { Add } from './icons';
 import { Accordion, AccordionPanel } from './accordion';
 import { Heading } from './heading';
+import { Tabs, Tab } from './tabs';
 
 export {
   Button,
   Add,
   Accordion, AccordionPanel,
-  Heading
+  Heading,
+  Tabs, Tab
 }
