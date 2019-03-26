233029178d9dcad8cfee6c329a92dd5d2eab76b5
XImOnTRiX <domenik.wenghofer@gmail.com>
2019-03-26 12:00:45 +0100

    added single story

--- src/stories/accordion.stories.js
+++ src/stories/accordion.stories.js
@@ -13,16 +13,19 @@ class Primary extends React.Component {
     return (
       <div className="container-big">
         <Accordion header={<Heading type={3} size="middle">Accordion Headline</Heading>}>
-          <AccordionPanel header={<Heading type={2} size="small">Panel Name</Heading>}>
+          <AccordionPanel header={<div className="padding"><Heading type={2} size="small">Panel Name</Heading></div>}>
           <div className="container-big">
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-            <Button outline label="more" />
           </div>
           </AccordionPanel>
           <AccordionPanel header={<Heading type={2} size="small">Panel Name</Heading>}>
           <div className="container-big">
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-            <Button outline label="more" />
+          </div>
+          </AccordionPanel>
+          <AccordionPanel>
+          <div className="container-big">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
           </div>
           </AccordionPanel>
         </Accordion>
@@ -31,5 +34,50 @@ class Primary extends React.Component {
   }
 }
 
+class Custom extends React.Component {
+  render() {
+    return (
+      <div className="container-big">
+        <Accordion
+          header={ <Heading type={3} color="primary" size="small">Custom styled Accordion</Heading> }
+        >
+          <AccordionPanel
+            className="bg-tertiary-light"
+            headerStyle="bg-primary padding"
+            contentStyle="bg-tertiary primary"
+            header={ <Heading type={5} color="primary-dark" size="small">First Panel Name</Heading> }
+          >
+          <div className="container-big">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+          </AccordionPanel>
+          <AccordionPanel header={<Heading type={2} size="small">Panel Name</Heading>}>
+          <div className="container-big">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+          </AccordionPanel>
+        </Accordion>
+      </div>
+    );
+  }
+}
+
+class Single extends React.Component {
+  render() {
+    return (
+      <div className="container-big">
+          <Accordion>
+            <AccordionPanel header="Panel Name">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </AccordionPanel>
+          </Accordion>
+
+      </div>
+    );
+  }
+}
+
 storiesOf('Accordion', module)
   .add('primary', () => (<Primary />))
+  .add('custom', () => (<Custom />))
+  .add('single', () => (<Single />))
